a35da54662b9d4a74367cc4eae9717d4273b8b68
Delhi Babu <rldb143@gmail.com>
2023-11-24 00:16:18 +0530

    all done...

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ async function getweatherdata()
 {
       try{
          let input=document.getElementById("search").value;
-         let contain=document.getElementsByClassName(".container")
          let temper=document.getElementById("temp")
          let head=document.getElementById("head")
          let humi=document.getElementById("humidity")
          let win=document.getElementById("wind")
          let img=document.getElementById("image")
-         let data=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=020ac31ecf3b82c081d818fd371fc20e`)
+         let data=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&units=Metric&appid=020ac31ecf3b82c081d818fd371fc20e`)
          console.log(data);
          data=await data.json() 
          console.log(data);
@@ -47,4 +46,10 @@ let btn=document.getElementById("btn")
 
 btn.addEventListener("click",getweatherdata)
 
+let inputfield=document.getElementById("search")
+inputfield.addEventListener("keypress",(e)=>{
+   if (e.key === "Enter") {
+      getweatherdata()
+   }
+})
 
